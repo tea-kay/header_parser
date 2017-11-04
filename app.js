339c7b068ead7487be51126fd84d1326ba96a49c
Timothy Kim <tkim1202@gmail.com>
2017-11-04 16:48:25 +0900

    Removed cors dependancy

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var cors = require('cors');
 
 var index = require('./routes/index');
 
@@ -18,7 +17,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
 
 app.use('/', index);
 
